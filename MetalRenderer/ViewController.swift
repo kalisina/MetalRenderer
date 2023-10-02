89e23025d869187d43f708d9146d5efacc432f43
Kali Sina <ali.sina89@yahoo.com>
2023-10-02 12:29:50 +0200

    Initial Commit

---
+++ MetalRenderer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalRenderer
+//
+//  Created by Triumph on 02/10/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
